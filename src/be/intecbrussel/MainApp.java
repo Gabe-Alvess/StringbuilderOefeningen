f6ec1251f57cc34dcce32f9ec4888bb82a43e68e
Biel-Alves <gabriel_vieira2018@hotmail.com>
2023-02-02 00:29:37 +0100

    done

---
+++ src/be/intecbrussel/MainApp.java
@@ -0,0 +1,12 @@
+package be.intecbrussel;
+
+import java.util.Random;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Github");
+        System.out.println("---- Oefning 1 ----");
+        // Maak een applicatie die kan controleren of een bepaald woord een palindroom is.
+
+    }
+}
