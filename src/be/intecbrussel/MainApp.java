1a0ece7cde987d26de0af141cae63dd11217f8ea
Gabriel Alves <111418442+Biel-Alves@users.noreply.github.com>
2023-02-05 12:50:24 +0100

    exercise done

--- src/be/intecbrussel/MainApp.java
+++ src/be/intecbrussel/MainApp.java
@@ -32,25 +32,50 @@ public class MainApp {
 
         System.out.println("---- StringBuilder - oefenreeks 1 ----");
 
-        System.out.println("Oefening - 1");
+        System.out.println("---- Oefening - 1 ----");
         // 1. Maak een stringBuilder aan en voeg een string toe. Print de stringBuilder naar de console.
-        StringBuilder string = new StringBuilder("Hello World!");
-        System.out.println(string);
+        StringBuilder str1 = new StringBuilder("Hello World!");
+        System.out.println(str1);
 
-        System.out.println("Oefening - 2");
+        System.out.println("---- Oefening - 2 ----");
         // 2. Gebruik de .length() eigenschap om de lengte van een string of stringBuilder te bepalen.
-        StringBuilder str1 = new StringBuilder("Hello World");
-        System.out.println(str1.length());
+        StringBuilder str2 = new StringBuilder("Hello World");
+        System.out.println(str2.length());
 
-        System.out.println("Oefening - 3");
+        System.out.println("---- Oefening - 3 ----");
         // 3. Gebruik de .substring() methode om een deel van een string of stringBuilder
         // te selecteren en print dit naar de console.
-        StringBuilder str2 = new StringBuilder("Hello World");
-        System.out.println(str2.substring(6, 11));
+        StringBuilder str3 = new StringBuilder("Hello World");
+        System.out.println(str3.substring(6, 11));
 
-        System.out.println("Oefening - 4");
+        System.out.println("---- Oefening - 4 ----");
         // 4. Gebruik de .delete() methode om een deel van een stringBuilder te verwijderen.
-        StringBuilder str3 = new StringBuilder("Hello World Wold");
-        System.out.println(str3.delete(12,17));
+        StringBuilder str4 = new StringBuilder("Hello World Wold");
+        System.out.println(str4.delete(12,17));
+
+        System.out.println("---- Oefening - 5 ----");
+        // 5. Gebruik de .insert() methode om een string toe te voegen aan een specifieke index in een stringBuilder.
+        StringBuilder str5 = new StringBuilder("Let's");
+        str5.insert(5, " go");
+        System.out.println(str5);
+
+        System.out.println("---- Oefening - 6 ----");
+        // 6. Gebruik de .replace() methode om een specifieke string te vervangen door een andere string in een stringBuilder.
+        StringBuilder strReplace = new StringBuilder("Hello world!");
+        strReplace.replace(0,12, "Hallo wereld!");
+        System.out.println(strReplace);
+
+        System.out.println("---- Oefening - 7 ----");
+        // 7. Gebruik de .toString() methode om de inhoud van een stringBuilder om te zetten naar een string.
+        StringBuilder str6 = new StringBuilder("Hello, my name is Gabriel.");
+        String str7 = str6.toString();
+        System.out.println(str7);
+
+        System.out.println("---- Oefening - 7 ----");
+        // 8. Gebruik de .append() methode om een string toe te voegen aan een stringBuilder.
+        StringBuilder str8 = new StringBuilder("Hi,");
+        String endOfSentence = " my name is Jos";
+        str8.append(endOfSentence);
+        System.out.println(str8);
     }
 }
